cf4a0ba0f414a46edf361e88848435b039a3df73
nicfro <nicolai.frost@gmail.com>
2017-05-02 21:00:14 +0200

    test

---
+++ ship.py
@@ -0,0 +1,11 @@
+class ship:
+    def __init__(self, hitpoints):
+        self.x = x
+        self.y = y
+        self.hitpoints = hitpoints
+        self.acceleration = 0
+        self.orientation = 0
+
+    
+
+    
